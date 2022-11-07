cdad4f7bac542cadfbcda028509cf4bfa95f351c
Pascal <98275355+delavegar25@users.noreply.github.com>
2022-11-07 09:51:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load("./space.jpg");
 scene.background = spaceTexture;
 
 function moveCamera(){
